18a2638b4f758c85b668ada0d7cdeab8fd87df18
jieungo <jieungogogo@gmail.com>
2022-01-09 02:46:10 +0900

    Scene, Camera and Renderer-2

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,50 +2,87 @@ import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene();
+const scene2 = new THREE.Scene();
+
 scene.background = new THREE.Color(0x4488ff);
 
-const camera = new THREE.PerspectiveCamera(
+const camera1 = new THREE.PerspectiveCamera(
     75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+    // window.innerWidth / window.innerHeight,
+    1, // 200/200 == 1
+    .1,
+    10
 )
-camera.position.z = 2
+const camera2 = new THREE.OrthographicCamera(-1,1,1,-1, .1, 10)
+const camera3 = new THREE.OrthographicCamera(-1,1,1,-1, .1, 10)
+const camera4 = new THREE.OrthographicCamera(-1,1,1,-1, .1, 10)
 
-const renderer = new THREE.WebGLRenderer() //다양한 renderer가 존재하지만 webgl이 가장 빠르며 대부분 서포트 해준다 (가장 기본)
-renderer.setSize(window.innerWidth, window.innerHeight) // innerWidth대신 200으로 바꾼다면? camera와 밑의 resize에서도 변경해줘야 200의 크기가 제대로 유지된다
-document.body.appendChild(renderer.domElement) //html에 다이나믹하게 캔버스를 자동으로 그려준다
+camera1.position.z = 2
+camera2.position.y = 1
+camera2.lookAt(new THREE.Vector3()) //위에서 아래로. 디폴트는 0 0 0
+camera3.position.z = -1
+camera3.lookAt(new THREE.Vector3(0,0,0)) //왼쪽부터
+camera4.position.x = 1
+camera4.lookAt(new THREE.Vector3(0,0,0)) //앞부터
 
-new OrbitControls(camera, renderer.domElement)
+const canvas1 = document.getElementById('c1') as HTMLCanvasElement
+const canvas2 = document.getElementById('c2') as HTMLCanvasElement
+const canvas3 = document.getElementById('c3') as HTMLCanvasElement
+const canvas4 = document.getElementById('c4') as HTMLCanvasElement
 
-const geometry = new THREE.BoxGeometry()
+const renderer1 = new THREE.WebGLRenderer({canvas:canvas1})
+renderer1.setSize(200,200)
+const renderer2 = new THREE.WebGLRenderer({canvas:canvas2})
+renderer2.setSize(200,200)
+const renderer3 = new THREE.WebGLRenderer({canvas:canvas3})
+renderer3.setSize(200,200)
+const renderer4 = new THREE.WebGLRenderer({canvas:canvas4})
+renderer4.setSize(200,200)
+
+// const renderer = new THREE.WebGLRenderer() //다양한 renderer가 존재하지만 webgl이 가장 빠르며 대부분 서포트 해준다 (가장 기본)
+// renderer.setSize(window.innerWidth, window.innerHeight) // innerWidth대신 200으로 바꾼다면? camera와 밑의 resize에서도 변경해줘야 200의 크기가 제대로 유지된다
+// document.body.appendChild(renderer.domElement) //html에 다이나믹하게 캔버스를 자동으로 그려준다
+
+new OrbitControls(camera1, renderer1.domElement)
+
+const geometry = new THREE.TorusKnotGeometry() //THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
 
 const cube = new THREE.Mesh(geometry, material)
+cube.scale.x = .5
+cube.scale.y = .5
+cube.scale.z = .5
 scene.add(cube)
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
+const cube2 = new THREE.Mesh(geometry, material)
+scene2.add(cube2)
+
+// window.addEventListener('resize', onWindowResize, false)
+// function onWindowResize() {
+//     camera.aspect = window.innerWidth / window.innerHeight
+//     camera.updateProjectionMatrix()
+//     renderer.setSize(window.innerWidth, window.innerHeight)
+//     render()
+// }
 
 function animate() {
     requestAnimationFrame(animate)
 
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
-
+    
+    cube2.rotation.y += 0.01
     render()
 }
 
 function render() {
-    renderer.render(scene, camera)
+    renderer1.render(scene, camera1)
+    renderer2.render(scene, camera2)
+    renderer3.render(scene, camera3)
+    renderer4.render(scene, camera4)
 }
 
 animate()
